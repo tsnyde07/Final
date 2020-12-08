9491b006b18db6d18dc3245e4ef1b257863ee169
ahnichols42 <75275694+ahnichols42@users.noreply.github.com>
2020-12-07 21:22:49 -0500

    added sql statements, message content checking and updated form

--- Send.cs
+++ Send.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace Chat
 {
@@ -20,7 +20,73 @@ namespace Chat
 
         private void Send_Load(object sender, EventArgs e)
         {
+            String constr = DatabaseConnect.connectionString;
+            SqlConnection con = new SqlConnection(constr);
 
+            String sel = "select usernames from user_details";
+            SqlDataAdapter Da = new SqlDataAdapter(sel, con);
+            DataSet ds = new DataSet();
+
+            Da.Fill(ds, "QueryResult_user_details");
+            usernames_listbox.DataSource = ds.Tables["QueryResult_user_details"];
+            usernames_listbox.DisplayMember = "usernames";
+
+            con.Close();
+        }
+
+        public Boolean checkForMessageContents()
+        {
+            if (tb_message.Text.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void bt_send_Click(object sender, EventArgs e)
+        {
+            
+            Boolean checkForMessage = checkForMessageContents();
+            if(checkForMessage == true)
+            {
+                string message = tb_message.Text;
+                string recipient = usernames_listbox.SelectedItem.ToString();
+                String constr = DatabaseConnect.connectionString;  // See DatabaseConnect Class in Form1.cs file- bottom
+                SqlConnection con = new SqlConnection(constr);  // create the database connecting
+                SqlCommand cmd = con.CreateCommand(); // Need to execute the query
+                try
+                {
+                    //form the SQL insert query using the given data
+                    string query = "insert into user_message_details values('" + recipient + "','" + message + ")";
+                    cmd.CommandText = query;
+                    con.Open(); // open the Database connection for insertion when done must close the connection to avoid issues
+                    Int32 returnFlag = (Int32)cmd.ExecuteNonQuery(); // execute the query, the function returns 0 if the insertion unsuccessful
+                    if (returnFlag > 0)
+                        MessageBox.Show("Message Sent");
+                    else
+                        MessageBox.Show("Message Failed to Send");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    con.Close();
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a message before sending");
+            }
         }
 
         private void bt_close_Click(object sender, EventArgs e)
@@ -28,17 +94,9 @@ namespace Chat
             this.Close();
         }
 
-        private void bt_send_Click(object sender, EventArgs e)
+        private void tb_message_TextChanged(object sender, EventArgs e)
         {
-            
-            //Should add contents to the database then close
-            MessageBox.Show("If implemented, message would be sent!");
-            this.Close();
-        }
-
-        private void tb_to_TextChanged(object sender, EventArgs e)
-        {
-            //should check database for username
+           
         }
     }
 }
